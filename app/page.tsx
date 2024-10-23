f2e4883c92d54d94819ca6b5b44d28ffac7e6af4
Anass Nammal <anassnammal@gmail.com>
2024-10-23 04:21:23 +0100

    testing next api routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+"use client";
 import Image from "next/image";
+import { use, useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    fetch("/api/tasks", { method: "GET" }).then((res) => res.json()).finally(console.log).catch(console.error);
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
