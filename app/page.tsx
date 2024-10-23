a9c5273d673013ec984ebd4ed7d460b56044e98f
Anass Nammal <anassnammal@gmail.com>
2024-10-23 22:34:16 +0100

    Update styling and add list item component

--- app/page.tsx
+++ app/page.tsx
@@ -9,23 +9,23 @@ export default function Home() {
 
 
   useEffect(() => {
-    fetch("/api/tasks?user_id=ft_002").then((res) => res.json()).then((data) => setTasks(data.tasks));
+    fetch("/api/tasks?user_id=ft_001").then((res) => res.json()).then((data) => setTasks(data.tasks));
   }, []);
 
   return (
-    <div className="flex flex-col gap-1 bg-purple-500 h-screen">
-      <div className="flex justify-center items-center bg-green-600 h-40 w-full">
-        <h1 className="text-4xl">
+    <div className="flex flex-col gap-1 h-screen">
+      <div className="flex justify-center items-center bg-slate-400 h-32 w-full shadow-current/50 shadow-md rounded-b-2xl mx-1">
+        <h1 className="text-4xl text-white font-bold">
           Task Manager App
         </h1>
       </div>
-      <div className="bg-blue-600 h-full w-full">
+      <div className="h-full w-full overflow-y-auto">
         <ul className="flex flex-col gap-1">
           <List tasks={tasks} />
-          <TaskForm />
+          {/* <TaskForm /> */}
         </ul>
       </div>
-      <div className="bg-red-600 h-40 w-full"></div>
+      <div className="bg-slate-400 h-10 w-full shadow-current/50 shadow-md"></div>
     </div>
   );
 }
